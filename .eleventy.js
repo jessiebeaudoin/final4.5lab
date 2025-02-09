d7136c6beb9eaccec5fa5707913b2698eef7c9ae
jessiebeaudoin <123115086+jessiebeaudoin@users.noreply.github.com>
2025-02-09 00:52:24 +0000

    adding 11ty configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("styles");
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
 };
